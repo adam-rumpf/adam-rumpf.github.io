b0cca5550e41a0ab8e3b0171687df67e81bc8982
Adam Rumpf <arumpf@hawk.iit.edu>
2019-09-07 21:14:57 -0400

    Testing external JavaScript.

---
+++ js/rps.js
@@ -0,0 +1,6 @@
+function rps_choice()
+{
+	var things = ['Rock', 'Paper', 'Scissor'];
+	var thing = things[Math.floor(Math.random()*things.length)];
+	document.getElementById("p1").innerHTML = "The computer chose: " + thing;
+}
