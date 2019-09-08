e9fc1869dec8f0b47612a99501eca8e5dd11f186
Adam Rumpf <arumpf@hawk.iit.edu>
2019-09-07 21:22:48 -0400

    Edits to test .js.

--- js/rps.js
+++ js/rps.js
@@ -1,8 +1,6 @@
-function rps_choice()
+function rps_choice(handle)
 {
 	var things = ['Rock', 'Paper', 'Scissor'];
 	var thing = things[Math.floor(Math.random()*things.length)];
-	document.getElementById("p1").innerHTML = "The computer chose: " + thing;
+	document.getElementById(handle).innerHTML = "The computer chose: " + thing;
 }
-
-rps_choice();
