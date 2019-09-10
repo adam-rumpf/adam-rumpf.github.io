1bc127d0d1886665e861204dbb4a77baacf824d1
Adam Rumpf <arumpf@hawk.iit.edu>
2019-09-10 04:40:00 -0400

    Eliminated test items.

--- js/rps.js
+++
@@ -1,6 +0,0 @@
-function rps_choice(handle)
-{
-	var things = ['Rock', 'Paper', 'Scissor'];
-	var thing = things[Math.floor(Math.random()*things.length)];
-	document.getElementById(handle).innerHTML = "The computer chose: " + thing;
-}
