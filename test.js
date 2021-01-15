11678b06e8b6477a64446f99128d9fed82313e72
adam-rumpf <adam.rumpf@gmail.com>
2021-01-14 19:16:46 -0500

    Attempting to call JavaScript using a dropdown menu.

--- test.js
+++ test.js
@@ -1,15 +1,26 @@
-// Set of spacer object IDs to be shown/hidden at the same time
-list = ["cobwebbing", "complex-operations"];
-
-// Show/hide all elements in the above list
 function toggle()
 {
-	for (var i = 0; i < list.length; i++)
-	{
-		x = document.getElementById(list[i]);
-		if (x.style.display === "none")
-			x.style.display = "block";
-		else
-			x.style.display = "none";
-	}
+	altert("!!!");
 }
+
+/*// Define dictionary of tag groups
+var group = {};
+group["all"] = ["cobwebbing", "complex-operations"];
+group["first"] = ["cobwebbing"];
+group["second"] = ["complex-operations"];
+group["none"] = [];
+
+// Show only elements in specified tag list
+function toggle()
+{
+	// Get selected tag
+	var t = document.getElementById("tags").value;
+	
+	// Hide everything
+	for (var i = 0; i < group["all"].length; i++)
+		document.getElementById(group["all"][i]).style.display = "none";
+	
+	// Show the elements from the selected list
+	for (var i = 0; i < group[t].length; i++)
+		document.getElementById(group[t][i]).style.display = "block";
+}*/
